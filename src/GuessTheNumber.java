396ec05486d752bebd641234bd2abe7cf132de7c
Luis Mendez <slpmendez60@gmail.com>
2025-11-03 15:18:17 -0500

    feat: Explorando los Bucles While

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1; // Número aleatorio entre 1 y 100
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int attempts = 0;
+
+        System.out.println("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+        // TODO: Add a while loop here to implement the guessing game
+        while (true) {
+    System.out.print("Enter your guess: ");
+    guess = scanner.nextInt();
+    attempts++;
+
+    if (guess < numberToGuess) {
+        System.out.println("Too low! Try again.");
+    } else if (guess > numberToGuess) {
+        System.out.println("Too high! Try again.");
+    } else {
+        System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+        break;
+    }
+}
+
+        scanner.close();
+    }
+}
+
