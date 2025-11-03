671a9fb810057b84d3a858c163f0f49437b00cbc
Luis Mendez <slpmendez60@gmail.com>
2025-11-03 15:06:37 -0500

    feat: Trabajando con Bucles For

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,13 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 7;
+        System.out.println("Multiplication table for " + number + ":");
+
+        // TODO: Add a for loop here to print the multiplication table
+        for (int i = 1; i <= 15; i++) {
+    int result = number * i;
+    System.out.println(number + " x " + i + " = " + result);
+}
+
+    }
+}
