40a88bb131a32c2f9e987c09d1e6595e76ffdf1f
Luis Mendez <slpmendez60@gmail.com>
2025-11-07 20:13:08 -0500

    feat: Desafio FizzBuzz

--- src/WeatherAdvisor.java
+++ src/WeatherAdvisor.java
@@ -1,18 +1,17 @@
 public class WeatherAdvisor {
     public static void main(String[] args) {
-        int temperature = 5; // Temperatura en grados Celsius
 
-        System.out.println("The temperature is " + temperature + "°C.");
-
-        // TODO: Agrega sentencias if-else aquí para dar consejos climáticos
-        if (temperature > 30) {
-    System.out.println("It's a hot day. Remember to stay hydrated!");
-} else if (temperature > 20) {
-    System.out.println("It's a nice warm day. Enjoy the weather!");
-} else if (temperature > 10) {
-    System.out.println("It's a bit cool. You might need a light jacket.");
-} else {
-    System.out.println("It's cold. Wear warm clothes!");
-}
+        // Recorre los números del 1 al 100
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
