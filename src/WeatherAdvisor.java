ff224736147c5ff9b1bbf94a942eac9b70ff1679
Luis Mendez <slpmendez60@gmail.com>
2025-11-07 20:14:51 -0500

    feat: Desafio FizzBuzz mejora

--- src/WeatherAdvisor.java
+++ src/WeatherAdvisor.java
@@ -1,17 +1,21 @@
 public class WeatherAdvisor {
     public static void main(String[] args) {
 
-        // Recorre los números del 1 al 100
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for (int i = 1; i <= 200; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzzBoom");
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
+            } else if (i % 7 == 0) {
+                System.out.println("Boom");
             } else {
                 System.out.println(i);
             }
         }
     }
 }
+
